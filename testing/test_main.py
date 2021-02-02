b4f283c0dd265348b85de014bd8573eac44b4220
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-02 16:57:01 +0000

    Improved tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -1,10 +1,21 @@
+try:
+    from AssCheck import funcchecks as fc
+except:
+
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AssCheck"])
+    from AssCheck import funcchecks as fc:w
+
 import unittest
 from main import *
 
 class UnitTests(unittest.TestCase) :
     def test_function(self) : 
-        num = "" 
+        inputs, outputs, num = [], [], ""
         for i in range(1,400) :
+            inputs.append((i,))
             if (i+10)%100==0 : num = num.replace("LXXX","IC").replace("IX","")
             elif i%100==0 : num = num.replace("IC","C").replace("IX","")
             elif (i+10)%50==0 : num = num.replace("XXX","IL").replace("IX","")
@@ -13,5 +24,7 @@ class UnitTests(unittest.TestCase) :
             elif i%10==0 : num = num.replace("IX","X")
             elif (i+1)%5==0 : num = num.replace("III","IV")
             elif i%5==0 : num = num.replace("IV","V")
-            else : num = num + "I" 
-            self.assertTrue( num==romanNumeral(i), "your function is not working correctly" )
+            else : num = num + "I"
+            inputs.append((i,))
+            outputs.append( num ) 
+        assert( fc.check_func( 'romanNumeral', inputs, outputs ) ) 
